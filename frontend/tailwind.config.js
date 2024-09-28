e79dbe5096fc518b3025a8bf4dcca542dab73696
ahja <ahzathenation@gmail.com>
2024-09-29 04:01:00 +0700

    selesai halaman dashboard dan formulir

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
+  content: [".public/src/index.html", "./public/**/*.{html,js}"],
   theme: {
     fontFamily: {
       sans: ["Arial", "sans-serif"],
